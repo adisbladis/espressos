a549bd65af373f9ac89cfdbd23a00b67e507ac9a
adisbladis <adisbladis@gmail.com>
2023-04-03 15:02:59 +1200

    Add embeddedproto to nix-shell

--- shell.nix
+++ shell.nix
@@ -1,9 +1,71 @@
 let
   pkgs = import <nixpkgs> { };
+  inherit (pkgs) lib;
+
+  embeddedproto = pkgs.callPackage
+    ({ stdenv
+     , python3
+     , fetchFromGitHub
+     , protobuf
+     }:
+      let
+        pname = "EmbeddedProto";
+        version = "3.3.1";
+
+        pythonEnv = python3.withPackages (ps: [
+          ps.jinja2
+          ps.markupsafe
+          ps.protobuf
+          ps.six
+          ps.toposort
+        ]);
+
+      in
+      stdenv.mkDerivation {
+        inherit pname version;
+
+        src = fetchFromGitHub {
+          repo = "EmbeddedProto";
+          owner = "adisbladis";
+          rev = "system-libs";
+          sha256 = "sha256-yNL0dBlsIJLvMVFYDKBmxVdZDjfezm/kge7r81y19Tg=";
+        };
+
+        nativeBuildInputs = [ pythonEnv protobuf ];
+
+        buildPhase = ''
+          runHook preBuild
+          python setup.py --system-libs
+
+          runHook postBuild
+        '';
+
+        installPhase = ''
+          runHook preInstall
+
+          mkdir -p $out/share/EmbeddedProto
+          mv generator $out/share/EmbeddedProto/
+
+          mkdir -p $out/bin
+          cat > $out/bin/protoc-gen-eams <<EOF
+          #!${pkgs.runtimeShell}
+          exec ${pythonEnv.interpreter} $out/share/EmbeddedProto/generator/protoc-gen-eams.py --protoc-plugin "$@"
+          EOF
+          chmod +x $out/bin/protoc-gen-eams
+
+          runHook postInstall
+        '';
+
+      })
+    { };
+
 in
 pkgs.mkShell {
   packages = [
     pkgs.platformio
-    pkgs.clang-tools  # clang-format
+    pkgs.clang-tools # clang-format
+
+    pkgs.protobuf
+    embeddedproto
   ];
 }
