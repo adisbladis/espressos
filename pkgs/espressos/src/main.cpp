acd5bc4455fb9c3a25b7f9b359565d1a60048117
adisbladis <adisbladis@gmail.com>
2023-04-26 14:59:06 +1200

    Move OTA handling outside of main file

--- pkgs/espressos/src/main.cpp
+++ pkgs/espressos/src/main.cpp
@@ -10,6 +10,7 @@
 #include "fsm/fsmlist.hpp"
 #include "fsm/machine.hpp"
 #include "logger.hpp"
+#include "ota.hpp"
 #include "pressure.hpp"
 #include "websocket.hpp"
 
@@ -47,35 +48,7 @@ void setup() {
   pConfig->setup();
 
   // Set up OTA
-  {
-    ArduinoOTA.setPort(2040);
-
-    ArduinoOTA.onStart([]() { logger->log(LogLevel::DEBUG, "OTA start"); });
-    ArduinoOTA.onEnd([]() { logger->log(LogLevel::DEBUG, "OTA end"); });
-    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-      // logger->log(LogLevel::DEBUG, "OTA progress: %u%%", pct);
-    });
-    ArduinoOTA.onError([](ota_error_t error) {
-      const char *message = "Unknown error";
-
-      switch (error) {
-      case OTA_AUTH_ERROR:
-        message = "Auth Failed";
-      case OTA_BEGIN_ERROR:
-        message = "Begin Failed";
-      case OTA_CONNECT_ERROR:
-        message = "Connect Failed";
-      case OTA_RECEIVE_ERROR:
-        message = "Receive Failed";
-      case OTA_END_ERROR:
-        message = "End Failed";
-      }
-
-      logger->log(LogLevel::ERROR, "OTA Error[%u]: %s", error, message);
-    });
-
-    ArduinoOTA.begin();
-  }
+  beginOTA(2040);
 
   // Start listening
   apiServer.begin();
@@ -90,8 +63,6 @@ void setup() {
 }
 
 void loop() {
-  auto config = pConfig->getConfig();
-
   bool isBrewing = MachineState::is_in_state<Brewing>();
   bool isPumping = MachineState::is_in_state<Pumping>();
   bool isOff = MachineState::is_in_state<Off>();
@@ -130,6 +101,7 @@ void loop() {
     apiServer.setPressure(pressure);
   }
 
-  ArduinoOTA.handle();
+  handleOTA();
+
   apiServer.loop();
 }
