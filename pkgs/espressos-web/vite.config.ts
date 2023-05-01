dcac3078d959088b2af6475a157b4de91f7871d6
adisbladis <adisbladis@gmail.com>
2023-05-01 20:15:51 +1200

    Build protobuf typescript bindings in a vite plugin

--- pkgs/espressos-web/vite.config.ts
+++ pkgs/espressos-web/vite.config.ts
@@ -1,13 +1,32 @@
-import { defineConfig } from "vite";
+import { Plugin, defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
+import { InputOptions } from "rollup";
+import * as child from "child_process";
 
 export default defineConfig({
   plugins: [
     solidPlugin(),
-    // (() => {
-    //   return {
-    //   }
-    // })
+
+    // Build protobuf
+    ((): Plugin => {
+      return {
+        name: "espressos-proto",
+        buildStart: async () => {
+          const p: child.ChildProcess = child.exec("npm run generate");
+          await new Promise((resolve) => {
+            p.stdout.on("data", (x) => {
+              process.stdout.write(x.toString());
+            });
+            p.stderr.on("data", (x) => {
+              process.stderr.write(x.toString());
+            });
+            p.on("exit", (code) => {
+              resolve(code);
+            });
+          });
+        },
+      };
+    })(),
   ],
   server: {
     port: 3000,
