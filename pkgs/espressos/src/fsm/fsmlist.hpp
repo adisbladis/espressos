8485ea6c0aa99c720019a90bb0f50b3b19425879
adisbladis <adisbladis@gmail.com>
2023-05-02 20:58:24 +1200

    Add mutex to send_event
    I want to make this project use both cores. The best way to do that is
    to make send_event thread safe so that the second core can change
    the state machine.
    
    The other alternative is to have send_event do some internal queueing, but this seems easier.

--- pkgs/espressos/src/fsm/fsmlist.hpp
+++ pkgs/espressos/src/fsm/fsmlist.hpp
@@ -2,6 +2,14 @@
 
 #include <tinyfsm.hpp>
 
+// On the RPi Pico W & friends using pico-sdk we might be running the state
+// transition on another core. Therefore we use a recursive mutex where each
+// core is considered an owner.
+#if defined(ARDUINO_ARCH_RP2040) && !defined(ARDUINO_ARCH_MBED)
+#include <pico/mutex.h>
+auto_init_recursive_mutex(send_event_mutex);
+#endif
+
 #include "backflush.hpp"
 #include "brew.hpp"
 #include "machine.hpp"
@@ -12,5 +20,13 @@ using fsm_list =
 
 // Dispatch event to all fsm_list classes
 template <typename E> void send_event(E const &event) {
+#if defined(ARDUINO_ARCH_RP2040) && !defined(ARDUINO_ARCH_MBED)
+  recursive_mutex_enter_blocking(&send_event_mutex);
+#endif
+
   fsm_list::template dispatch<E>(event);
+
+#if defined(ARDUINO_ARCH_RP2040) && !defined(ARDUINO_ARCH_MBED)
+  recursive_mutex_exit(&send_event_mutex);
+#endif
 }
