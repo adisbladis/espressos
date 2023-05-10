9c7603bcc15c29ae8252c5f4aeccf588b85899f1
adisbladis <adisbladis@gmail.com>
2023-05-10 14:29:25 +1200

    Use embeddedproto from the develop branch
    So I can drop my custom patches.

--- shell.nix
+++ shell.nix
@@ -26,31 +26,16 @@ let
 
         src = fetchFromGitHub {
           repo = "EmbeddedProto";
-          owner = "adisbladis";
-          rev = "system-libs";
-          sha256 = "sha256-yNL0dBlsIJLvMVFYDKBmxVdZDjfezm/kge7r81y19Tg=";
+          owner = "Embedded-AMS";
+          rev = "ddebda1f03aeee87b0d5d124300ad2c533f23fd9";  # develop
+          sha256 = "sha256-Lw3Q9gCNLNLyLfwCgFYl/sQeyAvYYJppI7wgFkKTsCU=";
         };
-
-        patches = [
-          (pkgs.fetchpatch {
-            name = "header-include-paths.patch";
-            url = "https://github.com/Embedded-AMS/EmbeddedProto/commit/c50c17d83493652355fd130734939b5e73509d28.patch";
-            sha256 = "sha256-6WqofVNGeQgaCqVEQ01v20KlqMKl8Trn8yGm0roqviM=";
-          })
-
-          (pkgs.fetchpatch {
-            name = "header-no-copyright-header.patch";
-            url = "https://github.com/Embedded-AMS/EmbeddedProto/commit/968baf0e893924d7cc6369cab0344b44a3325a92.patch";
-            sha256 = "sha256-GkRH7jT27l/g2ZFiDurhfRQ2ZJO/DlcmfXA7CMGMaoY=";
-          })
-        ];
 
         nativeBuildInputs = [ pythonEnv protobuf ];
 
         buildPhase = ''
           runHook preBuild
-          python setup.py --system-libs
-
+          protoc -I generator --python_out=generator embedded_proto_options.proto
           runHook postBuild
         '';
 
